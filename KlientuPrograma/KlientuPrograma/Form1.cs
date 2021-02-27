7a3a598ab21f5f044bbf06d8943d6a8a5c9f9630
Karolina-Jur <melsva31@gmail.com>
2021-02-27 23:05:12 +0200

    UI

--- KlientuPrograma/KlientuPrograma/Form1.cs
+++ KlientuPrograma/KlientuPrograma/Form1.cs
@@ -10,11 +10,66 @@ using System.Windows.Forms;
 
 namespace KlientuPrograma
 {
-    public partial class Form1 : Form
+    public partial class Langas : Form
     {
-        public Form1()
+        string vard, pav, past;
+        double dat;
+        
+        public Langas()
         {
             InitializeComponent(); // ....
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void data_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void vardas_TextChanged(object sender, EventArgs e)
+        {
+            vard = Convert.ToString(vardas.Text);
+        }
+
+        private void pavarde_TextChanged(object sender, EventArgs e)
+        {
+            pav = Convert.ToString(pavarde.Text);
+        }
+
+        private void gimimo_data_TextChanged(object sender, EventArgs e)
+        {
+            dat = Convert.ToDouble(gimimo_data.Text);
+            if (dat != null)
+                prideti.Enabled = true;
+        }
+
+        private void pastabos_TextChanged(object sender, EventArgs e)
+        {
+            past = Convert.ToString(pastabos.Text);
+        }
+
+        private void klientai_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void vardadieniai_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tikrinti_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void prideti_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
