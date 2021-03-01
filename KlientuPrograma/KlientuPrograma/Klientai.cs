6b35a0e3f8b42be0e58f336b78c04d85cf582233
martynasstogisktu <martynas.stogis@ktu.edu>
2021-03-01 20:28:16 +0200

    Pridėta klientų klasė ir jos metodai (išskyrus vardadienių ųvedimą).

---
+++ KlientuPrograma/KlientuPrograma/Klientai.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlientuPrograma
+{
+    class Klientai
+    {
+        string vardas;
+        string pavarde;
+        DateTime gimtadienis;
+        List<String> vardadieniai;
+        string pastabos;
+        public Klientai (string vardas, string pavarde, DateTime gimtadienis)
+        {
+            this.vardas = vardas;
+            this.pavarde = pavarde;
+            this.gimtadienis = gimtadienis;
+        }
+        public string GetVardas()
+        {
+            return vardas;
+        }
+        public string GetPavarde()
+        {
+            return pavarde;
+        }
+        public DateTime GetGimtadienis()
+        {
+            return gimtadienis;
+        }
+        public List<String> GetVardadieniai()
+        {
+            return vardadieniai;
+        }
+        public string GetPastabos()
+        {
+            return pastabos;
+        }
+        public void SetPastabos(string pastabos)
+        {
+            this.pastabos = pastabos;
+        }
+        public void ReadVardadieniai()
+        {
+
+        }
+    }
+}
